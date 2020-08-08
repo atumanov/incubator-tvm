911b738c3394ba8a0a5b7297172615cff8e8affe
Khang D Vu <kvu35@flubber1.crnch.gatech.edu>
2020-08-08 12:28:32 -0400

    checkpoint commit

--- tvm_env.sh
+++ tvm_env.sh
@@ -1,4 +1,4 @@
-export TVM_HOME=$(readlink -f ~/git/SRTML/tvm)
+export TVM_HOME=$(readlink -f .)
 export PYTHONPATH=$TVM_HOME/python:$TVM_HOME/topi/python:${PYTHONPATH}
 export TVM_PATH=$TVM_HOME
 export VTA_HW_PATH=$TVM_PATH/3rdparty/vta-hw
